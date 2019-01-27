97c8ed0f75943017db48aa8a1e2f38748b1f22b1
kashif <naqvishah582@gmail.com>
2019-01-27 15:55:02 +0500

    show multi data

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div> <h1>Hello world</h1><p>This is paragraph</p> </div>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// For displaying listed items remember write sub chaild in one tag other wise error let's see bellow
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+  , document.getElementById('root'));
+
+// hello world Show
+// ReactDOM.render(<div> <h1>Hello world</h1><p>This is paragraph</p> </div>, document.getElementById('root'));
 serviceWorker.unregister();
