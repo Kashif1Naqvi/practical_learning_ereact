e214546cd43050d4270770fe71291498b836e561
kashif <naqvishah582@gmail.com>
2019-01-27 16:13:21 +0500

    functional components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// functional components remembers that first letter must be a capital
+function MyApp(){
+  return(
+    <div>
+      <ol>
+        <li>name</li>
+        <li>program</li>
+        <li>Developer</li> 
+      </ol>
+      <ul>
+        <li>Kashif naqvi</li>
+        <li>BS Computer Science</li>
+        <li>Master in Development</li>
+      </ul>
+    </div>
+  )
+}
+// i can close this in single tag because this is reactJs
+ReactDOM.render(<MyApp />, document.getElementById('root'));
 
 // For displaying listed items remember write sub chaild in one tag other wise error let's see bellow
 
-ReactDOM.render(
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ul>
-  , document.getElementById('root'));
+// ReactDOM.render(
+//   <ul>
+//     <li>1</li>
+//     <li>2</li>
+//     <li>3</li>
+//   </ul>
+//   , document.getElementById('root'));
 
 // hello world Show
-// ReactDOM.render(<div> <h1>Hello world</h1><p>This is paragraph</p> </div>, document.getElementById('root'));
+// ReactDOM.render(
+// <div>
+//   <h1>Hello world</h1>
+//   <p>This is paragraph</p> 
+// </div>,
+//  document.getElementById('root'));
 serviceWorker.unregister();
