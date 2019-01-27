d9f1a97d98b3228fcc994ce0a326f2ec83d86761
kashif <naqvishah582@gmail.com>
2019-01-27 16:43:25 +0500

    seprate components

---
+++ src/components/MyInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MyInfo(){
+  return(
+      <div>
+          <h1>Kashif naqvi</h1>
+          <p>Hello i am computer scientist learn from code doing coding </p>
+            <li>Cricket</li>
+            <li>Footbal</li>
+            <li>Hocky</li>
+      </div>
+  );  
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MyInfo from './components/MyInfo';
+ReactDOM.render(<MyInfo/>,document.getElementById('root'));
 
-// functional components remembers that first letter must be a capital
-function MyApp(){
-  return(
-    <div>
-      <ol>
-        <li>name</li>
-        <li>program</li>
-        <li>Developer</li> 
-      </ol>
-      <ul>
-        <li>Kashif naqvi</li>
-        <li>BS Computer Science</li>
-        <li>Master in Development</li>
-      </ul>
-    </div>
-  )
-}
-// i can close this in single tag because this is reactJs
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+//----------------------------Funtional component start---------------------------
+
+// // functional components remembers that first letter must be a capital
+// function MyApp(){
+//   return(
+//     <div>
+//       <ol>
+//         <li>name</li>
+//         <li>program</li>
+//         <li>Developer</li> 
+//       </ol>
+//       <ul>
+//         <li>Kashif naqvi</li>
+//         <li>BS Computer Science</li>
+//         <li>Master in Development</li>
+//       </ul>
+//     </div>
+//   )
+// }
+// // i can close this in single tag because this is reactJs
+// ReactDOM.render(<MyApp />, document.getElementById('root'));
+//----------------------------Funtional component End---------------------------
+
+
+
+
+//----------------------------Display list start---------------------------
 
 // For displaying listed items remember write sub chaild in one tag other wise error let's see bellow
 
@@ -34,6 +43,14 @@ ReactDOM.render(<MyApp />, document.getElementById('root'));
 //   </ul>
 //   , document.getElementById('root'));
 
+
+
+//----------------------------Display list End---------------------------
+
+
+
+//----------------------------Show Hello world start---------------------------
+
 // hello world Show
 // ReactDOM.render(
 // <div>
@@ -41,4 +58,11 @@ ReactDOM.render(<MyApp />, document.getElementById('root'));
 //   <p>This is paragraph</p> 
 // </div>,
 //  document.getElementById('root'));
+
+
+
+
+
+//----------------------------Show Hello world End---------------------------
+
 serviceWorker.unregister();
